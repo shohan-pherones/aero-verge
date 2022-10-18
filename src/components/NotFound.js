c59b20039fe634c14d260c7e8816723b493934c8
shohan-pherones <shohan.sub.56@gmail.com>
2022-10-18 21:23:41 +0600

    not found added

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="h-screen p-20 flex flex-col items-start gap-5">
+      <h2 className="text-4xl font-medium">Page not found</h2>
+      <Link to="/" className="btn gap-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3"
+          />
+        </svg>
+        Back to home
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
