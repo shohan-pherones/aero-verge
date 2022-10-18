8483f778d660c1fd5d0d30d3a910313fe8c43f3f
shohan-pherones <shohan.sub.56@gmail.com>
2022-10-18 21:04:38 +0600

    footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+      <div>
+        <p>
+          Copyright © {new Date().getFullYear()} - All right reserved by Aero
+          Verge
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
