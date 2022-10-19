7a9c34da0c3a175b6ee49a58d6ecebf1d0b3e4b8
shohan-pherones <shohan.sub.56@gmail.com>
2022-10-20 00:24:35 +0600

    support form refined

--- src/components/Support.js
+++ src/components/Support.js
@@ -4,57 +4,76 @@ import { useHistory } from "react-router-dom";
 const Support = () => {
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
   const [isWrong, setIsWrong] = useState("");
+  const [message, setMessage] = useState("");
   const history = useHistory();
 
   const handleSupportForm = (e) => {
     e.preventDefault();
+
     if (fullName && email && message) {
       history.push("/feedback");
     } else {
-      setIsWrong("Wrong inputs");
+      setIsWrong("Invalid inputs!");
     }
   };
 
   return (
-    <form className="flex flex-col gap-10 items-center p-20  h-screen">
-      <h2 className={`text-4xl font-medium ${isWrong && "text-rose-500"}`}>
-        {!isWrong ? "Support center" : isWrong}
+    <section className="container mx-auto text-center min-h-screen py-20">
+      <h2
+        className={`text-4xl font-semibold mb-5 ${isWrong && "text-rose-500"}`}
+      >
+        {!isWrong ? "Need help?" : isWrong}
       </h2>
-      <div className="flex flex-col gap-5">
-        <input
-          type="text"
-          placeholder="Full name"
-          className={`input input-bordered w-96 ${
-            isWrong && "border-rose-500"
-          }`}
-          value={fullName}
-          onChange={(e) => setFullName(e.target.value)}
-        />
-        <input
-          type="email"
-          placeholder="Email address"
-          className={`input input-bordered w-96 ${
-            isWrong && "border-rose-500"
-          }`}
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <textarea
-          rows="5"
-          className={`textarea textarea-bordered w-96 text-base resize-none ${
-            isWrong && "border-rose-500"
-          }`}
-          placeholder="Your message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        ></textarea>
+      <div className="form-control w-full max-w-xs mx-auto flex flex-col gap-3">
+        <div>
+          <label className="label" htmlFor="full-name">
+            <span className="label-text">Full name</span>
+          </label>
+          <input
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+            id="full-name"
+            type="text"
+            placeholder="Sarah Parker"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
+        <div>
+          <label className="label" htmlFor="email">
+            <span className="label-text">Email address</span>
+          </label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            id="email"
+            type="email"
+            placeholder="hello@example.com"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
+        <div>
+          <label className="label" htmlFor="message">
+            <span className="label-text">Your message</span>
+          </label>
+          <textarea
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            id="message"
+            rows="5"
+            placeholder="Write your message"
+            className="textarea textarea-bordered w-full max-w-xs resize-none text-base"
+          />
+        </div>
+        <button
+          className="btn btn-primary mt-5"
+          type="submit"
+          onClick={handleSupportForm}
+        >
+          Submit
+        </button>
       </div>
-      <button className="btn" type="submit" onClick={handleSupportForm}>
-        Submit
-      </button>
-    </form>
+    </section>
   );
 };
 
