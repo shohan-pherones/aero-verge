55414e7d151cc741fc45b5d52dd848ee4f2f8787
shohan-pherones <shohan.sub.56@gmail.com>
2022-10-19 23:36:41 +0600

    sign up form added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content py-10">
       <div>
         <p>
           Copyright © {new Date().getFullYear()} - All right reserved by Aero
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -18,43 +18,61 @@ const SignUp = ({ receiverFormSubmission }) => {
   };
 
   return (
-    <form className="flex flex-col gap-10 items-center p-20  h-screen">
-      <h2 className={`text-4xl font-medium ${isWrong && "text-rose-500"}`}>
-        {!isWrong ? "Sign up here" : isWrong}
+    <section className="container mx-auto text-center min-h-screen py-20">
+      <h2
+        className={`text-4xl font-semibold mb-5 ${isWrong && "text-rose-500"}`}
+      >
+        {!isWrong ? "Sign up" : isWrong}
       </h2>
-      <div className="flex flex-col gap-5">
-        <input
-          type="text"
-          placeholder="First name"
-          className={`input input-bordered w-96 ${
-            isWrong && "border-rose-500"
-          }`}
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Last name"
-          className={`input input-bordered w-96 ${
-            isWrong && "border-rose-500"
-          }`}
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-        />
-        <input
-          type="email"
-          placeholder="Email address"
-          className={`input input-bordered w-96 ${
-            isWrong && "border-rose-500"
-          }`}
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+      <div className="form-control w-full max-w-xs mx-auto flex flex-col gap-3">
+        <div>
+          <label className="label" htmlFor="first-name">
+            <span className="label-text">First name</span>
+          </label>
+          <input
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            id="first-name"
+            type="text"
+            placeholder="Sarah"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
+        <div>
+          <label className="label" htmlFor="last-name">
+            <span className="label-text">Last name</span>
+          </label>
+          <input
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            id="last-name"
+            type="text"
+            placeholder="Parker"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
+        <div>
+          <label className="label" htmlFor="email">
+            <span className="label-text">Email address</span>
+          </label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            id="email"
+            type="email"
+            placeholder="hello@example.com"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
+        <button
+          className="btn btn-primary mt-5"
+          type="submit"
+          onClick={handleSignUp}
+        >
+          Create account
+        </button>
       </div>
-      <button className="btn" type="submit" onClick={handleSignUp}>
-        Submit
-      </button>
-    </form>
+    </section>
   );
 };
 
