ce8185b43029d343c6b3882f887e411925a22c14
shohan-pherones <shohan.sub.56@gmail.com>
2022-10-19 21:07:15 +0600

    not found page modified

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,22 +3,22 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <section class="bg-white dark:bg-gray-900 py-20 min-h-screen">
-      <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-        <div class="mx-auto max-w-screen-sm text-center">
-          <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-sky-600 dark:text-sky-500">
+    <section className="bg-white dark:bg-gray-900 min-h-screen py-20">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div className="mx-auto max-w-screen-sm text-center">
+          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-cyan-600 dark:text-cyan-500">
             404
           </h1>
-          <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
+          <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
             Page not found.
           </p>
-          <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
+          <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
             Sorry, we can't find that page. You'll find lots to explore on the
             home page.{" "}
           </p>
           <Link
             to="/"
-            class="inline-flex text-white bg-sky-600 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-sky-900 my-4"
+            className="inline-flex text-white bg-cyan-600 hover:bg-cyan-800 focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-cyan-900 my-4"
           >
             Back to Homepage
           </Link>
