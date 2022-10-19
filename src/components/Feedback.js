5745852ebb66b38f52bf0d85e66bdf21c625fd83
shohan-pherones <shohan.sub.56@gmail.com>
2022-10-19 23:59:16 +0600

    feedback page added

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -3,27 +3,13 @@ import { Link } from "react-router-dom";
 
 const Feedback = () => {
   return (
-    <div className="h-screen p-20 flex flex-col items-center gap-5">
-      <h2 className="text-4xl font-medium">Thanks for contacting us</h2>
-      <p>We'll give you a feedback into your mail</p>
-      <Link to="/" className="btn gap-2 bg-cyan-500 border-cyan-500">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3"
-          />
-        </svg>
-        Back to home
+    <section className="min-h-screen py-20 mx-auto container text-center flex flex-col gap-3">
+      <h2 className="text-4xl font-semibold">Thanks for reaching us out</h2>
+      <p>Your submission has been recorded, we'll contact you later.</p>
+      <Link to="/" className="btn btn-primary self-center mt-5">
+        Okay, got it
       </Link>
-    </div>
+    </section>
   );
 };
 
