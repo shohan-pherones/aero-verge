8651f4ac847d24d32194885916eeffb1c5c7048e
shohan-pherones <shohan.sub.56@gmail.com>
2022-10-19 22:16:50 +0600

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,44 +3,13 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
-      <div className="sm:flex sm:items-center sm:justify-between">
-        <Link to="/" className="flex items-center mb-4 sm:mb-0">
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Aero Verge
-          </span>
-        </Link>
-        <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
-          <li>
-            <Link to="/about" className="mr-4 hover:underline md:mr-6 ">
-              About
-            </Link>
-          </li>
-          <li>
-            <Link to="/categories" className="mr-4 hover:underline md:mr-6">
-              Categories
-            </Link>
-          </li>
-          <li>
-            <Link to="/drones" className="mr-4 hover:underline md:mr-6 ">
-              Drones
-            </Link>
-          </li>
-          <li>
-            <Link to="/support" className="hover:underline">
-              Support
-            </Link>
-          </li>
-        </ul>
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+      <div>
+        <p>
+          Copyright © {new Date().getFullYear()} - All right reserved by Aero
+          Verge
+        </p>
       </div>
-      <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-      <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-        © {new Date().getFullYear()}{" "}
-        <Link to="/" className="hover:underline">
-          Aero Verge
-        </Link>
-        . All Rights Reserved.
-      </span>
     </footer>
   );
 };
